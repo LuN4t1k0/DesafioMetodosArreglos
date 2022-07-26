c9e400aec5109ce0e102fbd07dfa6e93d8688d6e
Cristian Venegas <cristian.venegas@materoceano.com>
2022-07-26 00:01:51 -0400

    style : change name of terminarTarea to actualizarTarea

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,16 +1,15 @@
-
-let tRealizadas = document.querySelector('#tRealizadas');
+let tRealizadas = document.querySelector("#tRealizadas");
 let tTareas = document.querySelector("#tTareas");
-let tbody = document.querySelector('#contenido')
-let inputTarea = document.querySelector('#inputTarea')
-let btnAgregar = document.querySelector('#formTareas')
-const tareas = []
+let tbody = document.querySelector("#contenido");
+let inputTarea = document.querySelector("#inputTarea");
+let btnAgregar = document.querySelector("#formTareas");
+const tareas = [];
 
 const renderTareas = () => {
-  let html =""
-  let contador = 0
+  let html = "";
+  let contador = 0;
   for (const tarea of tareas) {
-    contador++
+    contador++;
     html += /*html*/ `
     <tr>
       <th scope="row">${contador}</th>
@@ -18,39 +17,55 @@ const renderTareas = () => {
         <td>${tarea.estado}</td>
         <td>
           <button class="btn btn-danger" onclick="eliminarTarea(${tarea.id})">ELIMINAR</button>
-          <button class="btn btn-success ms-1" onclick="terminaTarea(${tarea.id})">TERMINAR</button>
+          <button class="btn btn-success ms-1" onclick="actualizarTarea(${tarea.id})">TERMINAR</button>
         </td>
     </tr>`;
   }
-  contenido.innerHTML = html
-  tTareas.innerHTML = tareas.length
+  contenido.innerHTML = html;
+  tTareas.innerHTML = tareas.length;
 };
 
-
-
 const agregarTarea = () => {
-  let nuevaTarea = { id: Date.now(), descripcion: inputTarea.value, estado: "pendiente" }
-  tareas.push(nuevaTarea)
-  inputTarea.value = ""
-  renderTareas()
-}
+  if (inputTarea.value.length == 0) {
+    alert("Debes ingresar una tarea");
+  } else {
+    let nuevaTarea = {
+      id: Date.now(),
+      descripcion: inputTarea.value,
+      estado: "pendiente",
+    };
+    tareas.push(nuevaTarea);
+    inputTarea.value = "";
+    renderTareas();
+  }
+};
 
-const terminaTarea = (id) => {
-  const index = tareas.findIndex(t => t.id == id)
-  tareas[index] = {id : tareas[index].id, descripcion : tareas[index].descripcion, estado : "Finalizado"}
-  filtrarTareas()
-  renderTareas()
+const actualizarTarea = (id) => {
+  const index = tareas.findIndex((t) => t.id == id);
+  tareas[index] = {
+    id: tareas[index].id,
+    descripcion: tareas[index].descripcion,
+    estado: "Finalizado",
+  };
+  filtrarTareas();
+  renderTareas();
   console.log(tareas);
-}
+};
 
 const eliminarTarea = (id) => {
-  const index = tareas.findIndex(t => t.id === id)
-  tareas.splice(index, 1)
-  filtrarTareas()
-  renderTareas()
-}
+  const index = tareas.findIndex((t) => t.id === id);
+  tareas.splice(index, 1);
+  filtrarTareas();
+  renderTareas();
+};
 
 const filtrarTareas = () => {
-  tareasFinalizadas = tareas.filter(t => t.estado ==="Finalizado")
-  tRealizadas.innerHTML = tareasFinalizadas.length
-}
+  tareasFinalizadas = tareas.filter((t) => t.estado === "Finalizado");
+  tRealizadas.innerHTML = tareasFinalizadas.length;
+};
+
+const validarTarea = () => {
+  if (inputTarea.value.length == 0) {
+    alert("Debes ingresar una tarea");
+  }
+};
